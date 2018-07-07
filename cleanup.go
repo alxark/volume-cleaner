bb365c168024d58308ef59c9c94daaf5100f48b7
AlxArk <alxark@noreply.users.github.com>
2018-07-07 23:20:03 +0300

    added real removing

--- cleanup.go
+++ cleanup.go
@@ -5,6 +5,9 @@ import (
 	"time"
 	"io/ioutil"
 	"fmt"
+	"os"
+	"syscall"
+	"math"
 )
 
 type CleanupService struct {
@@ -12,6 +15,7 @@ type CleanupService struct {
 	Expiration int
 	Period     int
 	log        *logrus.Logger
+	Scanner    *Scanner
 }
 
 func NewCleanupService(cleanupDirectory string, expiration int, period int, logger *logrus.Logger) (srv *CleanupService) {
@@ -23,6 +27,7 @@ func NewCleanupService(cleanupDirectory string, expiration int, period int, logg
 	}
 
 	logger.Debugf("New logger initialized. Dir: %s", cleanupDirectory)
+	srv.Scanner = NewScanner(srv.log)
 
 	return
 }
@@ -30,7 +35,12 @@ func NewCleanupService(cleanupDirectory string, expiration int, period int, logg
 func (s *CleanupService) Run() {
 	s.log.Info("Starting cleanup service")
 
-	scanner := NewScanner(s.log)
+	removedDir := s.Directory + "/removed"
+
+	if _, err := os.Stat(removedDir); os.IsNotExist(err) {
+		os.Mkdir(removedDir, 0766)
+		s.log.Infof("Creating removed directories storage at %s", removedDir)
+	}
 
 	for {
 		files, err := ioutil.ReadDir(s.Directory)
@@ -41,7 +51,13 @@ func (s *CleanupService) Run() {
 		}
 
 		for _, dir := range files {
-			if dir.Name() == "removed" || !dir.IsDir() {
+			if dir.Name() == "removed" {
+				continue
+			}
+
+			if !dir.IsDir() {
+				s.log.Infof("Non directory found on dir level: %s", dir.Name())
+				os.Remove(s.Directory + "/" + dir.Name())
 				continue
 			}
 
@@ -54,21 +70,80 @@ func (s *CleanupService) Run() {
 			}
 
 			for _, subDir := range subFiles {
+				if !subDir.IsDir() {
+					s.log.Infof("Non directory found on subDir level: %s", subDir.Name())
+					os.Remove(s.Directory + "/" + dir.Name())
+					continue
+				}
+
 				fullName := fmt.Sprintf("%s/%s/%s", s.Directory, dir.Name(), subDir.Name())
 				s.log.Debugf("Checking directory %s", fullName)
 
-				expired := scanner.IsExpired(fullName, s.Expiration)
+				expired, bytes := s.Scanner.IsExpired(fullName, s.Expiration)
 				if !expired {
 					s.log.Infof("Directory %s is not expired", fullName)
 					continue
 				}
 
-				s.log.Infof("Going to move %s to removed folder", fullName)
+				s.log.Infof("Going to move %s to removed folder, total used bytes: %0.2f MB", fullName, float64(bytes)/(1024*1024))
+				newDirName := time.Now().Format("20060102-1504-") + dir.Name() + "_" + subDir.Name()
+
+				curFullPath := s.Directory + "/" + dir.Name() + "/" + subDir.Name()
+				newFullPath := removedDir + "/" + newDirName
+
+				s.log.Infof("Moving %s to %s", curFullPath, newFullPath)
+				os.Rename(curFullPath, newFullPath)
 			}
+
+			s.CleanupRemoved()
 		}
 
 		s.log.Infof("Scanning finished. Sleep for %d", s.Period)
 		time.Sleep(time.Duration(s.Period) * time.Second)
 	}
+}
+func (s *CleanupService) CleanupRemoved() {
+	usage, err := s.GetUsage()
+	if err != nil {
+		return
+	}
 
+	s.log.Infof("Total usage is %0.2f", usage)
+	if usage < 0.5 {
+		s.log.Infof("Not removing old files, total usage below 50%")
+		return
+	}
+
+	coef := int(math.Ceil(10.0 * (1.0 - usage)))
+	s.log.Infof("Expiration coefficient is %d", coef)
+	removed, err := ioutil.ReadDir(s.Directory + "/removed")
+
+	for _, dir := range removed {
+		s.log.Infof("Checking already removed dir: %s", dir.Name())
+		fullName := s.Directory + "/removed/" + dir.Name()
+		expired, bytes := s.Scanner.IsExpired(fullName, s.Expiration * coef)
+
+		if !expired {
+			s.log.Infof("Directory %s is not fully expired", fullName)
+			continue
+		}
+
+		s.log.Infof("Directory %s is fully expired. Going to free %0.2f MB", fullName, float64(bytes)/(1024*1024))
+		os.RemoveAll(fullName)
+	}
+}
+
+func (s *CleanupService) GetUsage() (float64, error) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(s.Directory, &fs)
+
+	if err != nil {
+		return 100, err
+	}
+
+	totalBytes := fs.Blocks * uint64(fs.Bsize)
+	availBytes := fs.Bfree * uint64(fs.Bsize)
+	usingBytes := totalBytes - availBytes
+
+	return float64(usingBytes) / float64(totalBytes) , nil
 }
--- scanner.go
+++ scanner.go
@@ -18,30 +18,35 @@ func NewScanner(log *logrus.Logger) (scanner *Scanner) {
 
 // check if there is any new files after expiration
 // threshold, if so return yes
-func (s *Scanner) IsExpired(directory string, expiration int) bool {
+func (s *Scanner) IsExpired(directory string, expiration int) (bool, int64) {
 	minimumTime := time.Now().Unix() - int64(expiration)
 
 	files, err := ioutil.ReadDir(directory)
 	if err != nil {
 		s.log.Debugf("failed to scan directory %s, got: %s", directory, err.Error())
-		return true
+		return true, 0
 	}
 
+	var bytesScanned int64
+
+	bytesScanned = 0
 	for _, file := range files {
 		if file.IsDir() {
-			if !s.IsExpired(directory+"/"+file.Name(), expiration) {
-				return false
+			if expired, _ := s.IsExpired(directory+"/"+file.Name(), expiration); !expired {
+				return false, 0
 			}
 
 			s.log.Debugf("Finished scanning %s/%s for files. No new files found", directory, file.Name())
 			continue
 		}
 
+
+		bytesScanned += file.Size()
 		if file.ModTime().Unix() > minimumTime {
 			s.log.Infof("file %s/%s has modification time %s", directory, file.Name(), file.ModTime().Format("Jan 2 15:04:05 -0700 MST 2006"))
-			return false
+			return false, 0
 		}
 	}
 
-	return true
+	return true, bytesScanned
 }
